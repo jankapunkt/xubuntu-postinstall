cd774a01457e34fd5fb1b10a5b9bdc9b14a45bda
Jan Küster <jkuester@uni-bremen.de>
2018-10-01 00:04:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,10 +19,17 @@ apt-get install -y
 git # - fast, scalable, distributed revision control system
 
 # ----------------------------------------------------------------------------------
-# Graphics and Design
+# Multimedia, Graphics and Design
 # ----------------------------------------------------------------------------------
 gimp     # - GNU Image Manipulation Program
 inkscape # - vector-based drawing program
+vlc      # - multimedia player and streamer
+
+# ----------------------------------------------------------------------------------
+# OFFICE
+# ----------------------------------------------------------------------------------
+libreoffice # - office productivity suite (metapackage)
+texlive     # - TeX Live: A decent selection of the TeX Live packages
 
 # ----------------------------------------------------------------------------------
 # Web
@@ -34,13 +41,18 @@ chromium-codecs-ffmpeg       # - Free ffmpeg codecs for the Chromium Browser
 chromium-codecs-ffmpeg-extra # - Extra ffmpeg codecs for the Chromium Browser
 chromium-ublock-origin       # - general-purpose lightweight ads, malware, trackers blocker (Chromium)
 libavcodec-extra             # - FFmpeg library with extra codecs (metapackage)
+thunderbird                  # - Email, RSS and newsgroup client with integrated spam filter
 
 # ----------------------------------------------------------------------------------
 # Utitlity Programs
 # ----------------------------------------------------------------------------------
 gnome-disk-utility # - manage and configure disk drives and media
+filezilla          # - Full-featured graphical FTP/FTPS/SFTP client
+synaptic           # - Graphical package manager
 
 # ----------------------------------------------------------------------------------
 # XFCE Environment
 # ----------------------------------------------------------------------------------
-plank # - Elegant, simple, clean dock
+plank         # - Elegant, simple, clean dock
+xfce4-goodies # - enhancements for the Xfce4 Desktop Environment
+
