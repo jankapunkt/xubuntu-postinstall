005bd7fc87a825884cfaae69606fe915a004caa2
Jan Küster <jkuester@uni-bremen.de>
2018-10-01 11:06:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
 `# ----------------------------------------------------------------------------------` \
-`# let's update our packages first
+`# let's update our packages first                                                   ` \
 `# ----------------------------------------------------------------------------------` \
 sudo apt-get update && sudo apt-get upgrade -y
 `# ----------------------------------------------------------------------------------` \
-`# first, we install from repositories
+`# first, we install from repositories                                               ` \
 `# ----------------------------------------------------------------------------------` \
 sudo apt-get install -y \
 `# ----------------------------------------------------------------------------------` \
-`# development
+`# development                                                                       ` \
 `# ----------------------------------------------------------------------------------` \
 git `# - fast, scalable, distributed revision control system` \
 `# ----------------------------------------------------------------------------------` \
-`# Multimedia, Graphics and Design
+`# Multimedia, Graphics and Design                                                   ` \
 `# ----------------------------------------------------------------------------------` \
 gimp     `# - GNU Image Manipulation Program` \
 inkscape `# - vector-based drawing program` \
 vlc      `# - multimedia player and streamer` \
 `# ----------------------------------------------------------------------------------` \
-`# OFFICE
+`# OFFICE                                                                            ` \
 `# ----------------------------------------------------------------------------------` \
 libreoffice `# - office productivity suite (metapackage)` \
 texlive     `# - TeX Live: A decent selection of the TeX Live packages` \
 `# ----------------------------------------------------------------------------------` \
-`# Web
+`# Web                                                                               ` \
 `# ----------------------------------------------------------------------------------` \
 chromium-browser             `# - Chromium web browser, open-source version of Chrome` \
 chromium-browser-l10n        `# - chromium-browser language packages` \
@@ -35,14 +35,14 @@ chromium-ublock-origin       `# - general-purpose lightweight ads, malware, trac
 libavcodec-extra             `# - FFmpeg library with extra codecs (metapackage)` \
 thunderbird                  `# - Email, RSS and newsgroup client with integrated spam filter` \
 `# ----------------------------------------------------------------------------------` \
-`# Utitlity Programs
+`# Utitlity Programs                                                                 ` \
 `# ----------------------------------------------------------------------------------` \
 gnome-disk-utility `# - manage and configure disk drives and media` \
 filezilla          `# - Full-featured graphical FTP/FTPS/SFTP client` \
 synaptic           `# - Graphical package manager` \
 curl               `# - command line tool for transferring data with URL syntax` \
 `# ----------------------------------------------------------------------------------` \
-`# XFCE Environment
+`# XFCE Environment                                                                  ` \
 `# ----------------------------------------------------------------------------------` \
 plank         `# - Elegant, simple, clean dock` \
 xfce4-goodies `# - enhancements for the Xfce4 Desktop Environment`
